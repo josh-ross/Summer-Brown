bc80d0018e6589fa19cbbd8c33373b79294582cf
Josh Ross <josh.ross1234@gmail.com>
2014-11-24 18:59:52 -0500

    final9.py

---
+++ final9.py
@@ -0,0 +1,9 @@
+# This is the script for question 9
+import random
+import time
+
+#random.randint(1,20)
+print "Hello! What's your name?"
+name=str(raw_input())
+print "%s ,Well, %s, I am thinking of a number between 1 and 20." %(name)
+print "Take a guess."
