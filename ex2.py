2022af5720c5d607a03fe7c5f0d5bf81a2f846b8
Josh Ross <josh.ross1234@gmail.com>
2014-11-24 18:58:53 -0500

    ex2.py

---
+++ ex2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# This is the script for HW1 question 5
+
+color=raw_input("What is your favorite color?\n")
+
+space=len(color)*8
+
+
+print color*10
+print color+(' '*space)+color
+print color+(' '*space)+color
+print color*10
