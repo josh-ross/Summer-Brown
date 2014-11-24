6b60b9fccf8a0dc73357dec850c037f0399d0156
Josh Ross <josh.ross1234@gmail.com>
2014-11-24 18:58:16 -0500

    final12.py

---
+++ final12.py
@@ -0,0 +1,10 @@
+# This is the script for question 12
+empty=[]
+print "How many numbers do you want to display?"
+amt=int(raw_input())
+print "What numbers would you like to display? (please separate the numbers by spaces)"
+numbers=int(raw_input())
+empty.append(numbers)
+
+amt=len(empty)
+print amt
