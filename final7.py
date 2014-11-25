415880191219ed8579eccebb4de673b4600f7594
Josh Ross <josh.ross1234@gmail.com>
2014-11-24 19:04:46 -0500

    final7.py

---
+++ final7.py
@@ -0,0 +1,18 @@
+# This is for question 7
+s=3
+i=5
+empty=[]
+'''while 0<s and i<1000:	
+	print s+i
+	s=s+3
+	i=i+5'''
+
+for s in range(1,1000):
+	print s
+	s=s+3
+for i in range(1,1000):
+	print i
+	i=i+5
+
+for tot in range(1,1000):
+	empty.append(tot)
